310f7ece02af2b6358b5705f1fd0f4742dfd0281
choffa <cgjovag@gmail.com>
2019-04-02 14:03:03 +0200

    Implement wgan-gp

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -98,21 +98,20 @@ class Pix2PixModel(BaseModel):
         pred_real = self.netD(real_AB)
         self.loss_D_real = self.criterionGAN(pred_real, True)
         # combine loss and calculate gradients
-        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+        grad_penalty = networks.cal_gradient_penalty(self.netD, real_AB, fake_AB, 'cuda')
+        self.loss_D = (self.loss_D_fake + self.loss_D_real + grad_penalty)
         self.loss_D.backward()
 
     def backward_G(self):
         """Calculate GAN and L1 loss for the generator"""
         # First, G(A) should fake the discriminator
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
-        real_AB = torch.cat((self.real_A, self.real_B), 1)
         pred_fake = self.netD(fake_AB)
-        grad_penalty = networks.cal_gradient_penalty(self.netD, real_AB, fake_AB, 'cuda')
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
-        self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
+        # self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
         # combine loss and calculate gradients
-        self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        self.loss_G = self.loss_G_GAN # + self.loss_G_L1
         self.loss_G.backward()
 
     def optimize_parameters(self):
