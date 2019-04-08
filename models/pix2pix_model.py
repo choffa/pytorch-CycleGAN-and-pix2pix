63a2c1ba93edf54294ff61add2c5cfad9a144c05
choffa <cgjovag@gmail.com>
2019-04-08 10:07:09 +0200

    Added if for wgangp

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -44,7 +44,7 @@ class Pix2PixModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake', 'penalty']
+        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake', 'penalty'] if opt.gan_mode in ['wgangp'] else ['G_GAN', 'G_L1', 'D_real', 'D_fake']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         self.visual_names = ['real_A', 'fake_B', 'real_B']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
@@ -105,9 +105,13 @@ class Pix2PixModel(BaseModel):
         self.loss_D_real = self.criterionGAN(pred_real, True)
         # print(self.loss_D_real)
         # combine loss and calculate gradients
-        self.loss_penalty = networks.gradient_penalty(self.netD, real_AB, fake_AB.detach(), 'cuda')
+        if self.opt.gan_mode in ['wgangp']:
+            self.loss_penalty = networks.gradient_penalty(self.netD, real_AB, fake_AB.detach(), 'cuda')
+            self.loss_D = (self.loss_D_fake + self.loss_D_real + self.loss_penalty)
+        else:
+            self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+
         # print(grad_penalty[0])
-        self.loss_D = (self.loss_D_fake + self.loss_D_real + self.loss_penalty)
         self.loss_D.backward()
 
     def backward_G(self):
@@ -119,7 +123,7 @@ class Pix2PixModel(BaseModel):
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
         # combine loss and calculate gradients
-        self.loss_G = self.loss_G_GAN # + self.loss_G_L1
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1
         self.loss_G.backward()
 
     def optimize_parameters(self):
